91b6a5fdcce0cd8eba9d90d51d5232c0a93d7ad4
simonallan <simon.allan@cancer.org.uk>
2019-03-28 17:40:38 +0000

    initial project version

---
+++ su2c-gbbo-log-resolver.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#set -o xtrace
+
+#servarr=('10227' '10228' '10229' '15242' '15243' '15244' '15245')
+servarr=('10227' '10228')
+
+
+#su2c@web-10227:/$ find /var/log/sites/su2c/logs/web-10227/ -name access.log-* -mtime -4 -mtime +1
+#/var/log/sites/su2c/logs/web-10227/access.log-20190325.gz
+#/var/log/sites/su2c/logs/web-10227/access.log-20190326.gz
+
+logpath='/var/log/sites/su2c/logs/web-10227/'
+destpath='/home/allan05/data/Logs/su2c-gbbo-stats'
+
+
+for s in "${servarr[@]}"
+do
+  ssh="ssh -oStrictHostKeyChecking=no -i ~/.ssh/allan05-acquia su2c.prod@web-$s.prod.hosting.acquia.com"
+  logpath="/var/log/sites/su2c/logs/web-$s"
+
+  $ssh logfiles=$(find $logpath -name access.log-* -mtime -4 -mtime +0) | echo $logfiles
+done
