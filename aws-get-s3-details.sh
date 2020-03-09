bf2209aa325289509a1ef235a509c6d4ab10448b
simonallan <simon.allan@cancer.org.uk>
2020-03-09 14:59:51 +0000

    add some new scripts

---
+++ aws-get-s3-details.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Quick and dirty scrtipt to iterate through AWS S3 buckets
+# and filch out some details
+
+logfile=/tmp/bucket-details.txt
+
+starttime=$(date +"%m-%d-%Y %T")
+
+# log all output
+exec 1>$logfile 2>&1
+
+# Account to check
+profile="awsprod"
+
+# List names of all buckets 
+buckets=$(aws s3api list-buckets --profile awsprod --query "Buckets[].Name" --output text)
+
+# Iterate over the list of S3 buckets, doing a thing to each one
+for b in $buckets; do
+  echo $b
+  aws s3api get-bucket-logging --bucket $b --profile $profile
+  echo ""
+done
+
+now=$(date +"%m-%d-%Y %T")
+echo "* * * Script started:  $starttime"
+echo "* * * Script complete: $now"
+
---
+++ get-adgroup-membership.ps1
@@ -0,0 +1,14 @@
+﻿# Simple script to get membership of all AWS Landing Zone AD groups
+
+Import-Module ActiveDirectory
+
+# Use naming convention to get LZ specific AD groups
+$lzadgroups=Get-ADGroup -Filter {name -like "ar - aws*"} | select samaccountname | sort Name
+
+ForEach ($g in $lzadgroups.samaccountname){
+  Write-host $g
+    Foreach ($m in $g){
+      Get-ADGroupMember $m | select name
+      }
+  Write-host ""
+  }
