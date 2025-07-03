6dd9a485215affe66e010d4a1a6e39e9fa46c423
Simon Allan <simon.allan@cancer.org.uk>
2025-07-03 16:26:19 +0100

    update bucket deletion script and general cleanup

--- aws-cli/s3-bucket-delete/find-s3-bucket-owner-stack.sh
+++ aws-cli/s3-bucket-delete/find-s3-bucket-owner-stack.sh
@@ -40,13 +40,14 @@ do
   # Strip the preceeding 'arn:aws:s3:::' from bucket ARN
   physicalId="$(echo $bucket | awk -F ':' '{print $6}')"
 
+  # Perform a stack resource lookup based on the resource ID
   stackName="$(aws cloudformation describe-stack-resources \
   --physical-resource-id "$physicalId" \
   --no-cli-pager \
   --output text \
   --query 'StackResources[0].{stackname: StackName, timeStamp: Timestamp}' 2> /dev/null )"
 
-  # command receives validation error 254 if a corresponding stack is not found.
+  # command receives validation error 254 "Validation Error" if a corresponding stack is not found.
   if [ $? == 254 ]; then
     output="$bucket - Stack does not exist"
     printf "$output\n" >> $output_file
@@ -55,7 +56,7 @@ do
     output="$bucket - created with stack $stackName"
     printf "$output\n" >> $output_file
     ((++hasStackCounter))
-    bucketsWithStacks[$end]+="$output"
+    bucketsWithStacks[$end]+="$output\n"
   fi
 done < $input_file
 
@@ -63,5 +64,8 @@ totalBuckets=$(($noStackCounter + $hasStackCounter))
 printf "\nTotal Buckets Checked: $totalBuckets\n\n"
 printf "Buckets with belonging to existing stack: $hasStackCounter\n"
 printf "Buckets with orphaned from creator stack: $noStackCounter\n\n"
-printf "The following should probably be checked and NOT deleted:\n"
-printf "$bucketsWithStacks"
+
+if [ $hasStackCounter -gt 0 ]; then
+  printf "The following should probably be checked and NOT deleted:\n"
+  printf "$bucketsWithStacks"
+fi
